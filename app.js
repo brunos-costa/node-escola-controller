68c327e87c539e3471418a631901548d728f163d
Bruno Costa <analistabrunocosta@gmail.com>
2024-03-26 14:53:24 -0300

    Criado rotas index e professor, bem como as views iniciais do projeto

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const handlebars = require('express-handlebars')
 const app = express()
 const porta = 3000
 
+// CARREGANDO AS ROTAS
+const indexRouter = require('./routes/index')
+const professorRouter = require('./routes/professor')
+
 // CONFIGURANDO O HANDLEBARS
 app.engine('handlebars', handlebars.engine())// Esta linha configura o Handlebars como mecanismo de visualização, registrando o motor de visualização Handlebars com a chave 
 app.set('view engine','handlebars')// Define o mecanismo de visualização padrão como Handlebars.
 app.set('views','./views')// Define o diretório de visualizações como ./views, onde o Express.js procurará por arquivos de visualização
 
 // ROTAS
-app.get('/',(req, res)=>{
-    //res.send("Tudo certo")
-    res.render('index')
-})
+app.use('/',indexRouter)
+app.use('/professor',professorRouter)
 
 
 //EXECUTANDO SERVIDOR
